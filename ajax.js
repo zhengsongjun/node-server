9599333afd60c24817b7a948e0baaa000ba402dc
zhengsongjun <568700485@qq.com>
2018-11-11 19:06:46 +0800

    静态服务器

--- ajax.js
+++
@@ -1,6 +0,0 @@
-var xhr = new XMLHttpRequest()
-xhr.open('GET','/getWeather',true)
-xhr.send()
-xhr.onload = function(){
-    console.log(JSON.parse(xhr.responseText))
-}
--- mock.js
+++ mock.js
@@ -1,19 +1,33 @@
 var http = require('http')
 var fs = require('fs')
+var url = require('url')
 
 http.createServer(function(req,res){
     if(req.url == '/favicon.ico'){
         return false
     }else{
-        switch(req.url){
+        var pathObj = url.parse(req.url,true)
+        switch(pathObj.pathname){
             case '/getWeather':
-                res.end(JSON.stringify({a:1,b:2}))
+                var ret
+                if(pathObj.query.city == 'beijing'){
+                    ret = {
+                        city:'北京',
+                        weather:'sum'
+                    }
+                }else{
+                    ret = {
+                        city:pathObj.query.city ,
+                        weather:'不知道'
+                    }
+                }
+                res.end(JSON.stringify(ret))
                 break
             case '/user/123':
                 res.end(fs.readFileSync(__dirname+'/img/1.jpg'))
                 break
             default:
-                res.write(fs.readFileSync(__dirname+req.url))
+                res.write(fs.readFileSync(__dirname+pathObj.pathname))
                 res.end()
         }
     }
